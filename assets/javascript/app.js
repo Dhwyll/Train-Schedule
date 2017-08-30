ebbf45c0b73eb422a3f20c4962260f58d7614149
Brian Evans <dhwyll@gmail.com>
2017-08-30 01:53:58 -0700

    Added time calculations, including if the train leaves right now

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,67 +1,80 @@
 // Initialize Firebase
 // Make sure to match the configuration to the script version number in the HTML
-// (Ex. 3.0 != 3.7.0)         
+// (Ex. 3.0 != 3.7.0)		 
 var config = {
-  apiKey: "AIzaSyCkdqplkBHCOLknnGXsESkNcK-RdwegZig",
-  authDomain: "my-first-project-cbc.firebaseapp.com",
-  databaseURL: "https://my-first-project-cbc.firebaseio.com",
-  projectId: "my-first-project-cbc",
-  storageBucket: "",
-  messagingSenderId: "886865820846"
+	apiKey: "AIzaSyCkdqplkBHCOLknnGXsESkNcK-RdwegZig",
+	authDomain: "my-first-project-cbc.firebaseapp.com",
+	databaseURL: "https://my-first-project-cbc.firebaseio.com",
+	projectId: "my-first-project-cbc",
+	storageBucket: "",
+	messagingSenderId: "886865820846"
 };
 
-firebase.initializeApp(config);
+firebase.initializeApp(config);											// Initialize the App
 
 
 // Create a variable to reference the database.
 var database = firebase.database();
-var employeeRef = database.ref("/trainData");
+var trainRef = database.ref("/trainData");
 
-var trainName, trainDest, trainStart, trainFreq;
-
-
-
+// Create variables to be used
+var trainName, trainDest, trainStart, trainFreq;						// These are the variables from the form
+var rightNow, duration, minutesDiff, nextMinutesDiff;					// These will be used to manage the time calculations
+																		// rightNow is the current time
+																		// duration is the amount of minutes between now and the start of the train in either direction
+																		// minutesDiff is duration converted to minutes
+																		// nextMinutesDiff is the number of minutes in the future of the next train
 // Add train
 
-$("#addTrain").on("click", function(event) {
-	event.preventDefault();
+$("#addTrain").on("click", function(event) {							// When the user adds a train
+	event.preventDefault();												// Prevent the default action of that Submit button
 	
-	trainName = $("#nameInput").val().trim();
-	trainDest = $("#destInput").val().trim();
-	trainStart = moment($("#startInput").val().trim(), "HH:mm");
-	trainFreq = $("#freqInput").val().trim();
+	trainName = $("#nameInput").val().trim();							// Get trainName from the form
+	trainDest = $("#destInput").val().trim();							// Get trainDest from the form
+	trainStart = moment($("#startInput").val().trim(), "HH:mm");		// Get trainStart from the form and convert to HH:mm format
+	trainFreq = $("#freqInput").val().trim();							// Get trainFreq from the form
 	
-	console.log("trainName is " + trainName);
-	console.log("trainDest is " + trainDest);
-	console.log("trainStart is " + moment(trainStart).format("HH:mm"));
-	console.log("trainFreq is " + trainFreq);
-
-	$("#nameInput").val("");
+	$("#nameInput").val("");											// Clear the form entries
 	$("#destInput").val("");
 	$("#startInput").val("");
 	$("#freqInput").val("");
+	
+	rightNow = moment(new Date(), "HH:mm");								// What time is it right now?
+	duration = moment.duration(rightNow.diff(trainStart));				// How much time between right now and when the train starts
+	minutesDiff = parseInt(duration.asMinutes());						// Convert the duration to minutes.
+	if (minutesDiff === 0) {											// If the train is leaving right now...
+		trainNext = "Right Now";												// Then set tNext to "Right Now"...
+		trainAway = 0;															// Which is 0 minutes away.
+	}
+		else if (minutesDiff < 0) {										// Otherwise, if the trains starts in the future...
+			trainNext = moment(trainStart).format("HH:mm");						// Then set tNext to the start time of the train...
+			trainAway = parseInt(duration.asMinutes()) * -1 + 1;				// Reverse polarity of duration adding 1 to account for 0 minutes and set tAway.
+		}
+			else {															// Otherwise, the train starts in the past
+				nextMinutesDiff = trainFreq - (minutesDiff % trainFreq);	// Get the remainder of minutes given the frequency of the train
+				trainNext = moment(rightNow).add(nextMinutesDiff, "minutes").format("HH:mm");		// Set tNext to now + nextMinutesDiff
+				trainAway = nextMinutesDiff;									// And set tAway to nextMinutesDiff
+			}
 
-	database.ref("/trainData").push({
+	database.ref("/trainData").push({									// Push the values to the database
 		tName: trainName,
 		tDest: trainDest,
 		tFreq: trainFreq,
-		tStart: moment(trainStart).format("HH:mm"),
-		tNext: 5, //moment calculation here;
-		tAway: 10 //moment calculation here;
+		tStart: moment(trainStart).format("HH:mm"),						// convert the trainStart time to HH:mm format
+		tNext: trainNext,
+		tAway: trainAway
 	});
 
 });
 
-employeeRef.on("child_added", function(childSnapshot) {
-	console.log("I got activated");
-
-	$('#trainTable tbody').append(
-		"<tr><td>" + childSnapshot.val().tName + "</td>" +
+trainRef.on("child_added", function(childSnapshot) {					// When the database gets a new train
+	$('#trainTable tbody').append(										// Append to the tbody of the trainTable
+		"<tr><td>" + childSnapshot.val().tName + "</td>" +				// The various variables from the database in their own cells
 		"<td>" + childSnapshot.val().tDest + "</td>" +
 		"<td>" + childSnapshot.val().tFreq + "</td>" +
 		"<td>" + childSnapshot.val().tNext + "</td>" +
 		"<td>" + childSnapshot.val().tAway + "</td>" +
 		"</tr>");	
-}, function(errorObject) {
+}, function(errorObject) {												// Unless there is an error.
 	console.log("Errors handled: " + errorObject.code);
 });
