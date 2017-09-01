55e0147ee94392e627ca33ca59ce500c4618bbd7
Brian Evans <dhwyll@gmail.com>
2017-08-31 17:17:31 -0700

    Added code to update schedule in real time

--- assets/javascript/appold.js
+++ assets/javascript/appold.js
@@ -74,64 +74,13 @@ $("#addTrain").on("click", function(event) {							// When the user adds a train
 });
 
 trainRef.on("child_added", function(childSnapshot) {					// When the database gets a new train
-	var childKey = childSnapshot.key;
 	$('#trainTable tbody').append(										// Append to the tbody of the trainTable
 		"<tr><td>" + childSnapshot.val().tName + "</td>" +				// The various variables from the database in their own cells
 		"<td>" + childSnapshot.val().tDest + "</td>" +
 		"<td>" + childSnapshot.val().tFreq + "</td>" +
-		"<td id='"+childKey+"Next'>" + childSnapshot.val().tNext + "</td>" +
-		"<td id='"+childKey+"Away'>" + childSnapshot.val().tAway + "</td>" +
-		"</tr>");
+		"<td>" + childSnapshot.val().tNext + "</td>" +
+		"<td>" + childSnapshot.val().tAway + "</td>" +
+		"</tr>");	
 }, function(errorObject) {												// Unless there is an error.
 	console.log("Errors handled: " + errorObject.code);
 });
-
-
-setInterval(function(){
-	trainRef.on('value', function(snapshot) {
-		snapshot.forEach(function(childSnapshot) {
-			var childData = childSnapshot.val();
-			var childKey = childSnapshot.key;
-			var updateData;
-			var updates = {};
-			
-			trainStart = moment(childData.tStart, "HH:mm");
-			trainFreq = childData.tFreq;
-
-			rightNow = moment(new Date(), "HH:mm");								// What time is it right now?
-			timeBetween = moment.duration(rightNow.diff(trainStart));			// How much time between right now and when the train starts
-			minutesDiff = parseInt(timeBetween.asMinutes());						// Convert the timeBetween to minutes.
-			if (minutesDiff === 0) {											// If the train is leaving right now...
-				trainNext = "Right Now";												// Then set trainNext to "Right Now"...
-				trainAway = 0;															// Which is 0 minutes away.
-			}
-				else if (minutesDiff < 0) {										// Otherwise, if the trains starts in the future...
-					trainNext = moment(trainStart).format("HH:mm");						// Then set tNext to the start time of the train...
-					trainAway = parseInt(timeBetween.asMinutes()) * -1 + 1;				// Reverse polarity of timeBetween adding 1 to account for 0 minutes and set tAway.
-				}
-					else {															// Otherwise, the train starts in the past
-						nextMinutesDiff = trainFreq - (minutesDiff % trainFreq);	// Get the remainder of minutes given the frequency of the train
-						if (nextMinutesDiff === trainFreq) {						// If the train is leaving right now...
-							trainNext = "Right Now";									// Then set trainNext to "Right Now"...
-							trainAway = 0;												// Which is 0 minutes away.
-						}
-							else {
-								trainNext = moment(rightNow).add(nextMinutesDiff, "minutes").format("HH:mm");		// Set tNext to now + nextMinutesDiff
-								trainAway = nextMinutesDiff;														// And set tAway to nextMinutesDiff
-							}
-					}
-
-			updateData = {tAway: trainAway,
-						  tDest: childData.tDest,
-						  tFreq: childData.tFreq,
-						  tName: childData.tName,
-						  tNext: trainNext,
-						  tStart: childData.tStart};
-			updates['/' + childKey] = updateData;
-			trainRef.update(updates);
-			$("#"+childKey+"Next").html(childData.tNext);
-			$("#"+childKey+"Away").html(childData.tAway);
-		});
-	});
-
-}, 5000);
